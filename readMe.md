3cb3927135a5f8245556b626d3532ca98e99eb1b
Stefano Falda <stefano.falda@gmail.com>
2023-05-20 23:09:06 +0200

    Added basic express configuration

--- readMe.md
+++ readMe.md
@@ -1,2 +1,6 @@
 ### Template source
 https://blog.appsignal.com/2022/01/19/how-to-set-up-a-nodejs-project-with-typescript.html
+
+### Sqlite libraries
+    "better-sqlite-pool": "^0.3.1",
+    "better-sqlite3": "^8.1.0",
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,47 @@
-function sayMyName(name: string): void {
-    if (name === "Ste") {
-        console.log("You're right 👍");
-    } else {
-        console.log("You're wrong 👎");
-    }
-}
+import express from 'express';
+/*import { DatabaseRepository } from './helpers/database_repository';
+import admin from './routes/admin';
+import api_scraper from './routes/api_scraper';
+import login from './routes/login';
+import scripts from './routes/scripts';
+*/
+import base from './routes/base';
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+app.use(cors());
 
-sayMyName("Ste");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// add router in the Express app.
+app.use("/", base);
+//app.use("/api", api_scraper);
+
+// Admin interface
+//app.use("/admin", admin);
+
+// Register & Login
+//app.use("/", login);
+
+// Scripts
+//app.use("/", scripts);
+
+
+
+// Default to port 3000 but allow a PORT variable to be set
+// es. PORT=8000 npm run serve
+var port = process.env.PORT || 3000;
+
+
+app.listen(port, function () {
+    console.log(`Sync Server listening on port ${port}!`);
+
+});
+
+process.setMaxListeners(50);
+//DatabaseRepository.checkDB();
+
