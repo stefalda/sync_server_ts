033a914b42ea3ca3c64b9d201ff9b1595d277ecd
Stefano Falda <stefano.falda@gmail.com>
2023-05-20 23:01:20 +0200

    Initial commit

---
+++ readMe.md
@@ -0,0 +1,2 @@
+### Template source
+https://blog.appsignal.com/2022/01/19/how-to-set-up-a-nodejs-project-with-typescript.html
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+function sayMyName(name: string): void {
+    if (name === "Ste") {
+        console.log("You're right 👍");
+    } else {
+        console.log("You're wrong 👎");
+    }
+}
+
+sayMyName("Ste");
