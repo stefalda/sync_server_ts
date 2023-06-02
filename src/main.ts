7c737c1cc78f3140770adf568b299909f81ad0bc
Stefano Falda <stefano.falda@gmail.com>
2023-06-02 23:11:49 +0200

    Added logging with morgan

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import express from 'express';
 import * as configJson from '../config.json';
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, '..', 'log')
+});
 
 /*import { DatabaseRepository } from './helpers/database_repository';
 import admin from './routes/admin';
@@ -12,6 +21,9 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
+// Log calls
+app.use(morgan('[:date[iso]] :method :url :status :res[content-length] - :response-time ms - :remote-addr - :remote-user', { stream: accessLogStream }));
+
 // Accept big payload
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
