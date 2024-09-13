7b965101cc566b88d6364db89ea22126858edb46
Stefano Falda <stefano.falda@gmail.com>
2024-09-13 18:38:09 +0200

    Fixes id type in users table

--- database_cockroachdb_script.sql
+++ database_cockroachdb_script.sql
@@ -186,7 +186,7 @@ GRANT ALL ON TABLE public.user_tokens TO stefano WITH GRANT OPTION;
 -- DROP TABLE public."users";
 
 CREATE TABLE public.users (
-	id INT8 NOT NULL DEFAULT unique_rowid(),
+	userid UUID NOT NULL,
 	name VARCHAR(255) NULL,
 	email VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL,
